5d499c5b7c6f8437e6ec7c0f73e5fb77f9ec49db
Hamza Qureshi - Desktop <hamza7771.861@gmail.com>
2024-12-13 01:25:40 +0500

    Signed-off-by: Hamza Qureshi - Desktop <hamza7771.861@gmail.com>

---
+++ core/tests/parser_test.py
@@ -0,0 +1,14 @@
+
+import  json
+from core.parser.utils import json_loader, ConfigHolder, ConfigData
+
+JSON_PATH = "C:/Users/DELL/Desktop/datadecryption_poetry/core/settings.json"
+if __name__ == "__main__":
+    config_holder = json_loader(JSON_PATH)
+
+    # Load config data
+    with open(JSON_PATH, "r") as f:
+        data = json.load(f)
+    config = ConfigData(**data)
+    config_holder = ConfigHolder.from_config(config)
+    print(config_holder.DISP.imsi)
